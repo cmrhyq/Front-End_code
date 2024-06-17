d6d5b5b424ef247bcdb4e85a01f683cda7e86d40
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-06-17 23:20:47 +0800

    1.navigation

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,9 +1,25 @@
-import Fruits from "./components/state/managerState/Fruits";
+import {Routes, Route, Link} from 'react-router-dom';
+import HomePage from "./components/navigate/HomePage";
+import AboutPage from "./components/navigate/AboutPage";
+import "./App.css"
+import ContentPage from "./components/navigate/ContentPage";
+
 
 function App() {
     return (
-        <Fruits/>
-    );
-};
+        <div>
+            <nav className="nav">
+                <Link to="/" className="nav-item">Homepage</Link>
+                <Link to="/content" className="nav-item">Content</Link>
+                <Link to="/about" className="nav-item">About</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/content" element={<ContentPage/>}/>
+                <Route path="/about" element={<AboutPage/>}/>
+            </Routes>
+        </div>
+    )
+}
 
 export default App;
