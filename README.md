e9d1eaaba031e7e780ad98e5680a6ec211b929b3
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-06-20 22:52:04 +0800

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+<div style="font-family: 'Kanit', sans-serif;text-align: center;border: 10px solid #fff;box-shadow: 1px 1px 2px #e6e6e6;background: linear-gradient(to left top, #11998e, #38ef7d); padding: 50px 0;">
+<div style="color: #fff;">
+    <h3 style="font-size: 25px;font-weight: 600;letter-spacing: 1px;text-transform: uppercase;margin: 0;">
+       Front-end Common Code
+    </h3>
+    <span style="font-size: 16px;text-transform: capitalize;">
+    	前端常用代码
+    </span>
+</div>
+</div>
+
+本库包含前端的一些常用的、有特点的代码
--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,23 +1,9 @@
-import {Routes, Route, Link} from 'react-router-dom';
-import HomePage from "./components/navigate/HomePage";
-import AboutPage from "./components/navigate/AboutPage";
 import "./App.css"
-import ContentPage from "./components/navigate/ContentPage";
 
 
 function App() {
     return (
         <div>
-            <nav className="nav">
-                <Link to="/" className="nav-item">Homepage</Link>
-                <Link to="/content" className="nav-item">Content</Link>
-                <Link to="/about" className="nav-item">About</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/content" element={<ContentPage/>}/>
-                <Route path="/about" element={<AboutPage/>}/>
-            </Routes>
         </div>
     )
 }
